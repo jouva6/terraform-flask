2d23c9d26fedb6267164a0d498e08f861e478951
root <root@localhost.localdomain>
2025-09-19 16:01:11 +0200

    V2 - Flask modifié avec /status

--- main.tf
+++ main.tf
@@ -19,7 +19,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Flask V1</h1>'
+    return '<h1>Flask V2</h1>'
+@app.route('/status')
+def status():
+    return jsonify(status='running', version='V2')
 app.run(host='0.0.0.0', port=8080)
 EOT
 }
